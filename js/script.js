70ea838ef3e3186aabb8570f8e79436d6d616d81
serhii-kalyna <sergii_kalyna@mail.ru>
2017-07-09 21:21:57 +0300

    1

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+	$('#show-map').click(function(){
+		$('#hide-map').fadeIn(200);
+	});
+
+	$('#hide-map').click(function(){
+		$('#hide-map').fadeOut(200);
+	});
+
+})
